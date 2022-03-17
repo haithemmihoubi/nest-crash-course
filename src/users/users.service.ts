a6d5f1c12ab73189ebdb330696a9e35331121dd6
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-17 17:43:19 +0100

    genrating  users service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { User } from "./entities/user.entity";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [{ id: 0, name: 'haithem' }];
+  create(createUserDto: CreateUserDto) {
+    return this.users.push(createUserDto);
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users[id];
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
